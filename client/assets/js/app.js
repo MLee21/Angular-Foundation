d5d85073e68e29bf3c188144ec042bd366675627
MLee21 <minnie.lee89@gmail.com>
2015-08-25 21:19:34 -0600

    added films controller.

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -10,6 +10,37 @@
     'foundation.dynamicRouting',
     'foundation.dynamicRouting.animations'
   ])
+  .controller('FilmsCtrl', [
+    "$scope",
+    "$state",
+    "$http",
+    function($scope, $state, $http) {
+      $scope.id = ($state.params.id || '');
+      $scope.page = ($state.paramsp || 1);
+      if ($scope.page == 1) {
+        if ($scope.id != '') {
+          $http.get("http://swapi.co/api/"+'films'+"/"+$scope.id,
+            { cache: true })
+          .success(function(data) {
+            $scope['film'] = data;
+          })
+        } else {
+          $http.get("http://swapi.co/api/"+'films'+"/", { cache: true })
+          .success(function(data) {
+            $scope['films'] = data;
+            if (data['next']) $scope.nextPage = 2;
+          });
+        }
+      } else {
+        $http.get("http://swapi.co/api/"+'films'+"/?page="+$scope.page,
+          { cache: true }).success(function(data) {
+            $scope['films'] = data;
+            if (data['next']) $scope.nextPage = 1*$scope.page + 1;
+          });
+          $scope.prevPage = 1*$scope.page - 1;
+      }
+      return $scope;
+    }])
     .config(config)
     .run(run)
   ;
